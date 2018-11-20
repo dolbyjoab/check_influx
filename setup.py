b41995694294fa3a7afb0e79a62de43a984ad500
Etienne Delsy <dolbyjoab@gmail.com>
2018-11-20 03:43:44 -0800

    Add the original config

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
  
 setup(
-    name='check_influx',
-    version="1.1.1",
+    name='influx-nagios-plugin',
+    version="1.1.0",
     packages=['src'],
     install_requires=['influxdb', 'NagAconda'],
     license='MIT',
-    url="https://github.com/dolbyjoab/check_influx.git",
+    url="https://github.com/shaharke/nagios-influx-plugin.git",
     description='Nagios plugin for querying stats from InfluxDB',
-    author='Etienne Delsy',
-    author_email='dolbyjoab at gmail com',
+    author='Shahar Kedar',
+    author_email='shahar at gmail com',
     scripts=['src/check_influx']
 )
